6c57acb8e5b9e6585bb852297b0d5bc15a909202
Balttazarr <felekmakuta@wp.pl>
2021-06-06 23:39:09 +0200

    Update BaseGradeBook to abstract class

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -8,7 +8,6 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public string Name { get; set; }
         public RankedGradeBook(string name) : base(name)
         {
             Name = name;
